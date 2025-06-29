5a7d1b76c4c7f5f27808c21e1d70e0633288a6af
Siampc <tahasinahmedsiam20@gmail.com>
2025-06-29 21:51:47 +0600

    just a commit

---
+++ index.js
@@ -0,0 +1,23 @@
+import express, { request, response } from  "express"
+import {mongoDBURL, PORT} from "./config.js"
+import mongoose from 'mongoose'
+
+const app = express()
+
+app.get('/', (request, response) =>{
+    console.log(request);
+    return response.status(234).send("Welcome to Paragon");
+});
+
+
+mongoose
+    .connect(mongoDBURL)
+    .then(()=>{
+        console.log("App connected to database");
+        app.listen(PORT, () => {
+        console.log('App is listening to port: ' + PORT);
+        });
+    })
+    .catch((error)=>{
+        console.log(error);
+    });
